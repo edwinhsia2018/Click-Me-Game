92ad647e93d0d85ac57f5c3ef6add42be25c157a
Edwin Hsia <edwinhsia@gmail.com>
2018-05-02 23:11:46 -0500

    Added navbar and updated app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import CharCard from "./components/CharCard";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
 import Chars from "./characters.json";
+import Nav from "./components/Nav";
 import "./App.css";
 
 class App extends Component {
   state = {
-    Char: Chars,
+    Chars: Chars,
     score: 0,
     highScore: 0
   }
 
   updateScore = () => {
-    this.setState((newState) => ({ 
-      score: this.newState.score + 1}), 
+    this.setState((newState) => ({
+      score: this.newState.score + 1
+    }),
       () => this.winGame());
   }
 
+  newHighScore = () => {
+    this.setState((newState) => ({
+      highScore: newState.score
+    }))
+  }
+
   winGame = () => {
-    if (this.state.score == this.state.Chars.length){
+    if (this.state.score == this.state.Chars.length) {
       alert("Congratulations!  You Won!!");
       this.setState({});
       this.resetGame();
@@ -30,30 +37,58 @@ class App extends Component {
   }
 
   resetGame = () => {
-    this.setState({ score: 0});
-    this.state.Chars.forEach(element => {
-      
-    });((image))
+    this.setState({ score: 0 });
+    this.state.Chars.forEach((image) => {
+      image.clicked = false;
+    })
   }
 
-  randomShuffle = () => {
-
+  randomShuffle = (array) => {
+    let shuffleArr = [], n = array.length, i;
+    while (n) {
+      i = Math.floor(Math.random() * array.length);
+      if (i in array) {
+        shuffleArr.push(array[i]);
+        delete array[i];
+        n--;
+      }
+    }
+    this.setState({ Chars: shuffleArr})
   }
 
-  render () {
+  randomRender = id => {
+    this.state.Chars.forEach((image) => {
+      if (image.id === id) {
+        if (image.clicked) {
+          alert("You clicked on the same card twice!  You lost!");
+          this.setState({})
+          this.resetGame();
+          return false;
+        }
+        else {
+          this.updateScore();
+          image.clicked = true;
+        }
+        if (this.state.score >= this.state.highScore) {
+          this.newHighScore();
+        }
+      }
+    })
+  }
+
+  render() {
     return (
       <Wrapper>
-        <Title>Click Me Game</Title>
-        <Title>Current Score: {this.state.score}</Title>
-        {this.state.Chars.map(Chars => (
-          <CharCard
-            id={Chars.id}
+        <Nav score={this.state.score} highScore={this.state.highScore} />
+        {this.state.Chars.map(character => {
+          return <CharCard
+            {...character}
             key={Chars.id}
-            name={Chars.name}
-            image={Chars.image}
-            />
-        ))}
-        </Wrapper>
+            randomRender={this.randomRender}
+            randomOrganize={() => this.randomShuffle(this.state.Char)}
+          />
+        })}
+      </Wrapper>
     )
   }
 }
