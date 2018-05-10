7340bac4cc037d405f5dcd6b038d82de1cd20928
Edwin Hsia <edwinhsia@gmail.com>
2018-05-09 20:04:28 -0500

    Updated Styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Nav.css';
 
 const Nav = props => (
-    <nav className="navbar navbar-default" >
+    <nav className="jumbotron text-center" >
         <ul>
-            <h1 className="brand">Click Me Game</h1>
+            <h1 className="display-4 text-center"><strong>Lord of the Rings - Click Me Game</strong></h1>
+            <br></br>
             <ul>Click on an image to begin! <strong>Current Score: {props.score} | Highest Score: {props.highScore} </strong> </ul>
         </ul>
     </nav>
